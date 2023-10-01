5f0571bc7120e78843dff689e62bc57aaf71b1dd
Chibueze <arisachibueze@gmail.com>
2023-10-01 09:42:04 +0100

    Fixed issue with chakra menus

--- src/services/Api-client.ts
+++ src/services/Api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "588941c3b44c433a92ba2d79c50a6986"
-  }
-})
+    key: "588941c3b44c433a92ba2d79c50a6986",
+  },
+});
