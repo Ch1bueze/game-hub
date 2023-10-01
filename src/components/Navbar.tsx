64ae9ba5fd8cfe76496231c630125ed93d869672
Chibueze <arisachibueze@gmail.com>
2023-10-01 09:50:01 +0100

    Built search input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import Logo from "../assets/GameHub Resources/Logo/logo.webp"
-import ColorModeSwitch from './ColorModeSwitch'
-
-
+import { HStack, Image } from "@chakra-ui/react";
+import Logo from "../assets/GameHub Resources/Logo/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding={"10px"}>
-      <Image src={Logo} boxSize={'60px'}/>
-      <ColorModeSwitch/>
+    <HStack padding={"10px"}>
+      <Image src={Logo} boxSize={"60px"} />
+      <SearchInput />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
