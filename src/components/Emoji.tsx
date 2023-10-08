fb2a2f0b6ebd307c071ad5513e9cba00b86c1110
Chibueze <arisachibueze@gmail.com>
2023-10-08 08:39:18 +0100

    refactor game grid

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -10,13 +10,13 @@ interface Props {
 const Emoji = ({ rating }: Props) => {
   if (rating < 3) return null;
 
-  const emojiMap: {[key: number]: ImageProps} = {
+  const emojiMap: { [key: number]: ImageProps } = {
     3: { src: Meh, alt: "Meh", boxSize: "25px" },
     4: { src: ThumbsUp, alt: "recommended", boxSize: "25px" },
     5: { src: BullsEye, alt: "Exceptional", boxSize: "35px" },
   };
 
-  return <Image {...emojiMap[rating]} marginTop={1}/>;
+  return <Image {...emojiMap[rating]} marginTop={1} />;
 };
 
 export default Emoji;
