c4f354c984e3942a133e066ebed4a8391d527cbc
Chibueze Arisa <113350283+Ch1bueze@users.noreply.github.com>
2024-11-08 17:48:24 +0100

    Update App.tsx
    Removed unused HStack declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
